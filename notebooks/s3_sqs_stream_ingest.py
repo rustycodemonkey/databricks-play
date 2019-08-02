7056043819399a5edbebf07c02e92bb81f20bc88
Phillip Kim <philkim78@gmail.com>
2019-08-02 10:11:59 +1000

    Creation of streaming dataframe. Need to fix the schema.

--- notebooks/s3_sqs_stream_ingest.py
+++ notebooks/s3_sqs_stream_ingest.py
@@ -1,1 +1,13 @@
 # Databricks notebook source
+from pyspark.sql.types import *
+
+demo_bus_rain_schema = StructType().add("b", IntegerType())
+
+demo_bus_rain_df = spark.readStream \
+  .format("s3-sqs") \
+  .option("fileFormat", "json") \
+  .option("queueUrl", "https://sqs.ap-southeast-2.amazonaws.com/421411388928/mypersonalsqs") \
+  .schema(demo_bus_rain_schema) \
+  .load()
+
+# demo_bus_rain_df
