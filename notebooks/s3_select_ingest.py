048129e2335c0d2b820451a532c1556ad0b321d6
Phillip Kim <philkim78@gmail.com>
2019-08-15 19:07:42 +1000

    Had enough play with s3 select

--- notebooks/s3_select_ingest.py
+++ notebooks/s3_select_ingest.py
@@ -31,8 +31,8 @@ dbutils.library.list()
 
 # COMMAND ----------
 
-#dbutils.library.installPyPI('boto3', version="1.9.207")
-#dbutils.library.installPyPI('botocore', version="1.12.207")
+#dbutils.library.installPyPI('boto3', version="1.9.208")
+#dbutils.library.installPyPI('botocore', version="1.12.208")
 #dbutils.library.restartPython()
 
 # COMMAND ----------
@@ -57,7 +57,7 @@ client = boto3.client('s3', 'ap-southeast-2')
 
 response = client.select_object_content(
   Bucket='mypersonaldumpingground',
-  Key='vehicle_position/20190814/16/vehpos_p0.1565765202444.json',
+  Key='vehicle_position/20190814/16/vehpos_p0.1565765223425.json',
   Expression="""select
   s.body.id,
   s.body.vehicle.vehicle.id as veh_id,
@@ -95,3 +95,48 @@ for event in response['Payload']:
 
 # COMMAND ----------
 
+import sys
+print(sys.version)
+
+# COMMAND ----------
+
+# MAGIC %sh
+# MAGIC #/databricks/pypy3/bin/pip install py4j
+# MAGIC #/databricks/pypy3/bin/pip list
+# MAGIC #env | grep PY
+# MAGIC less /databricks/spark/conf/spark-env.sh
+
+# COMMAND ----------
+
+# MAGIC %sh
+# MAGIC #add-apt-repository ppa:pypy/ppa
+# MAGIC #apt-get clean
+# MAGIC #apt-get update
+# MAGIC #apt-get install -y pypy3 pypy3-lib pypy3-dev pypy3-tk
+# MAGIC #/usr/bin/pypy3 -V
+# MAGIC #cd /databricks
+# MAGIC #/usr/bin/virtualenv -p /usr/bin/pypy3 pypy3
+# MAGIC #source /databricks/pypy3/bin/activate
+# MAGIC #/databricks/pypy3/bin/python -V
+# MAGIC #which python
+# MAGIC pwd
+
+# COMMAND ----------
+
+import os
+os.environ["PYSPARK_PYTHON"]="/databricks/pypy3/bin/pypy3"
+dbutils.library.restartPython()
+
+# COMMAND ----------
+
+# MAGIC %sh
+# MAGIC #/databricks/pypy3/bin/pip install py4j
+# MAGIC #/databricks/pypy3/bin/pip list
+# MAGIC #env | grep PY
+# MAGIC #less /databricks/spark/conf/spark-env.sh
+# MAGIC #/databricks/pypy3/bin/pip list
+# MAGIC #rm /databricks/python
+# MAGIC #ln -s /databricks/pypy3 /databricks/python
+# MAGIC ls -l /local_disk0/pythonVirtualEnvDirs/virtualEnv-5ffeb90f-b094-4b6a-ac90-8df5ef9588e6/bin
+# MAGIC #cat /local_disk0/tmp/1565857814233-0/PythonShell.py
+# MAGIC #apt list python3-s*
